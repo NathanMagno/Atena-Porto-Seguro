2334bb2bf9daf1200e3f507889a7a845b81acd30
nathan <nathan.consolo17@gmail.com>
2024-11-02 14:38:08 -0300

    Tela de integrantes finalizada

--- porto-seguro-api/src/components/RodapeIntegrantes/RodapeIntegrantes.tsx
+++ porto-seguro-api/src/components/RodapeIntegrantes/RodapeIntegrantes.tsx
@@ -1,5 +1,14 @@
+import { useRouter } from "next/navigation"
+import Nuvem from "@/icons/icons8-nuvem-50.png"
+import Image from "next/image";
+
 export default function RodapeIntegrantes() {
 
+  const nav = useRouter();
+
+  const irHome = () => {
+    nav.push('/')
+  }
 
  const navegar = () => {
     return (
@@ -10,25 +19,25 @@ export default function RodapeIntegrantes() {
 
   return (
     <>
-      {/* <StyledRodapeIntegrantes>
+       <footer className="RodapeIntegrantes">
 
 
 
 
 
-        <StyledRepositorioContainer onClick={navegar}>
+        <div className="RepositorioContainer" onClick={navegar}>
 
           <p>Repositório Remoto</p>
 
-          <StyledCloud/>
-        </StyledRepositorioContainer>
+          <Image alt="Icone de nuvem" src={Nuvem}/>
+        </div>
 
         <nav>
-          <StyledLink to="/home">Página Inicial</StyledLink>
+          <p className="Li" onClick={irHome}>Página Inicial</p>
         </nav>
 
 
-      </StyledRodapeIntegrantes> */}
+      </footer>
 
 
     </>
